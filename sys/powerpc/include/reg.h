df8dd7918cf823ae03e9a6365257e66db2b11f4d
Luis Pires <luis.pires@eldorado.org.br>
2018-05-21 16:00:17 -0300

    Included VSX registers on powerpc core dumps

--- sys/powerpc/include/reg.h
+++ sys/powerpc/include/reg.h
@@ -27,6 +27,10 @@ struct vmxreg {
 	uint32_t vscr;
 };
 
+struct vsxreg {
+	uint64_t vshr[32];
+};
+
 struct dbreg {
 	unsigned int	junk;
 };
@@ -48,6 +52,10 @@ struct fpreg32 {
 
 struct vmxreg32 {
 	struct vmxreg data;
+};
+
+struct vsxreg32 {
+	struct vsxreg data;
 };
 
 struct dbreg32 {

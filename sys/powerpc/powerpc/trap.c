2d2c4576fd0395ef8c07f363b2c1b64be1004edc
Alfredo Dal'Ava Junior <alfredo@freebsd.org>
2020-09-04 11:11:15 +0000

    [PowerPC] print debugging information on trap signal

--- sys/powerpc/powerpc/trap.c
+++ sys/powerpc/powerpc/trap.c
@@ -153,6 +153,11 @@ static struct powerpc_exception powerpc_exceptions[] = {
 	{ EXC_LAST,	NULL }
 };
 
+static int uprintf_signal;
+SYSCTL_INT(_machdep, OID_AUTO, uprintf_signal, CTLFLAG_RWTUN,
+    &uprintf_signal, 0,
+    "Print debugging information on trap signal to ctty");
+
 #define ESR_BITMASK							\
     "\20"								\
     "\040b0\037b1\036b2\035b3\034PIL\033PRR\032PTR\031FP"		\
@@ -490,6 +495,14 @@ trap(struct trapframe *frame)
 		ksi.ksi_code = (int) ucode; /* XXX, not POSIX */
 		ksi.ksi_addr = (void *)addr;
 		ksi.ksi_trapno = type;
+		if (uprintf_signal) {
+			uprintf("pid %d comm %s: signal %d code %d type %d "
+				"addr 0x%lx r1 0x%lx srr0 0x%lx srr1 0x%lx\n",
+			p->p_pid, p->p_comm, sig, ucode, type, addr,
+			frame->fixreg[1], frame->srr0, frame->srr1);
+		}
+
+
 		trapsignal(td, &ksi);
 	}
 

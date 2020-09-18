4ad0b18dfee10e99c95fb70656a0fc32c811ee6d
Alfredo Dal'Ava Junior <alfredo@freebsd.org>
2020-09-18 18:10:41 +0000

    force clean of FE0 and FE1

--- sys/powerpc/powerpc/trap.c
+++ sys/powerpc/powerpc/trap.c
@@ -424,7 +424,7 @@ trap(struct trapframe *frame)
 				//register_t msr = mfmsr();
 				//msr &= ~PSL_RI;
 				//mtmsr(msr);
-				frame->srr1 &= ~PSL_RI;
+				//frame->srr1 &= ~PSL_RI;
 				frame->srr1 &= ~PSL_FE0;
 				frame->srr1 &= ~PSL_FE1;
 
